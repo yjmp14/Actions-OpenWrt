d31f6da2b94ef2870a0c796ae895fc813e8701a6
yjmp14 <yjmp14@gmail.com>
2020-04-22 14:46:26 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,5 +7,9 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+#!/bin/bash
+mkdir package/base-files/files/config
 echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
 echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
+./scripts/feeds update -a && ./scripts/feeds install -a
